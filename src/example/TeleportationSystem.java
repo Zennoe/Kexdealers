b8329f60e153be941d22a030ee617181c1435c48
Zennoe <agrar.pfurtz@gmail.com>
2018-08-02 04:22:11 +0200

    teleportation bug fix

--- src/example/TeleportationSystem.java
+++ src/example/TeleportationSystem.java
@@ -16,82 +16,81 @@ import ecs.Transformable;
 import loaders.BlueprintLoader;
 
 public class TeleportationSystem extends AbstractSystem {
-	
+
 	private HashMap<String, Teleportation> teleportations;
 
 	public TeleportationSystem(MessageBus messageBus, EntityController entityController) {
 		super(messageBus, entityController);
-		
+
 		teleportations = new HashMap<>();
 	}
 
 	public void run() {
 		// control update rate here
-		
+
 		// update :)
 		update();
-		
-		//cleanUp();
+
+		// cleanUp();
 	}
-	
+
 	protected void update() {
 		super.timeMarkStart();
-		
+
 		// work message queue
 		TeleportationSysMessage message;
-		while((message = (TeleportationSysMessage) messageBus.getNextMessage(Systems.TELEPORTATION_SYSTEM)) != null) {
-			switch(message.getOP()) {
-			case SYS_TELEPORTATION_TARGETCOORDS: teleportTo(message.getTargetEID(), message.getDestination());
+		while ((message = (TeleportationSysMessage) messageBus.getNextMessage(Systems.TELEPORTATION_SYSTEM)) != null) {
+			switch (message.getOP()) {
+			case SYS_TELEPORTATION_TARGETCOORDS:
+				teleportTo(message.getTargetEID(), message.getDestination());
 				break;
-			default: System.err.println("Teleportation operation not implemented");
+			default:
+				System.err.println("Teleportation operation not implemented");
 			}
 		}
 		// for all player entities,
 		// check all teleportations
-		
+
 		for (FPPCameraComponent player : entityController.getFPPCameraComponents()) {
 			for (Teleportation tp : teleportations.values()) {
 				Transformable transformable = entityController.getTransformable(player.getEID());
 				if (tp.checkTrigger(transformable)) {
-					teleportTo(player.getEID(),tp.getDestination());
+					teleportTo(player.getEID(), tp.getDestination());
 				}
 			}
 		}
-		
+
 		super.timeMarkEnd();
 	}
-	
-	protected void cleanUp() {
-		
-	}
-	
-	public void loadBlueprint(ArrayList<String> blueprint) {
-		ArrayList<String> teleLines = BlueprintLoader.getAllLinesWith("TELEPORTER", blueprint);
-		
-		for (String teleLine : teleLines) {
-						int eID = -1;
 
+	protected void cleanUp() {
+
+	}
+
+	public void loadBlueprint(ArrayList<String> blueprint) {
+		ArrayList<String> teleLines = BlueprintLoader.getAllLinesWith("TELEPORTATION", blueprint);
+
+		for (String teleLine : teleLines) {
 			try {
-			// extract data and add teleportation
+				// extract data and add teleportation
 				String[] frags = BlueprintLoader.getDataFragments(teleLine);
 				teleportations.put(frags[0], new Teleportation(frags[0], // name
 						new Vector3f(Float.valueOf(frags[1]), Float.valueOf(frags[2]), Float.valueOf(frags[3])), // destination
 						new Vector3f(Float.valueOf(frags[4]), Float.valueOf(frags[5]), Float.valueOf(frags[6])), // triggerLocation
 						Float.valueOf(frags[7]))); // triggerradius
-				
+
 			} catch (NullPointerException | IndexOutOfBoundsException e) {
 				System.err.println("Teleportations: couldn't load teleportation. Too few arguments.");
 			} catch (IllegalArgumentException e) {
-				System.err.printf("Teleportations: couldn't load teleportation. %s%n", eID, e.toString());
+				System.err.printf("Teleportations: couldn't load teleportation. %s%n", e.toString());
 			}
-	
 		}
 	}
-	
+
 	public Set<String> getAllTeleportations() {
 		return teleportations.keySet();
 	}
-	
+
 	private void teleportTo(int targetEID, Vector3f destination) {
 		// > fancy effects <
 		// wheeeeeee~~~
